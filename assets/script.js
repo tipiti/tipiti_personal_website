fbe86ae26461e1c1b77f29e87c294c0ac8c17f37
PHÚC THẠNH <you@example.com>
2021-09-22 15:54:06 +0700

    upload about section

--- assets/script.js
+++ assets/script.js
@@ -11,3 +11,19 @@ $(document).ready(function() {
     $(".navbar-menu").toggleClass("active");
   });
 });
+
+function myFunction() {
+  var dots = document.getElementById("dots");
+  var moreText = document.getElementById("more");
+  var btnText = document.getElementById("readmore-btn");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "đọc thêm";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "thu gọn";
+    moreText.style.display = "inline";
+  }
+}
